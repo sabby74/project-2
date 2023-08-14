ead40ab1bc9f837203887c3f75f98a4ebfa689fb
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2023-08-14 02:03:28 -0400

    login successfull

--- server.js
+++ server.js
@@ -50,15 +50,28 @@ app.get("/login", (req,res)=>{
 })
 
 
-app.post("/login", (req,res) =>{
+app.post("/login",async (req,res) =>{
   console.log(req.body);
-  res.send(req.body)
+  let userToLogin = await User.findOne({username: req.body.username })
+  //just to compare the passwords entered with req.body
+  if(userToLogin){
+    if(userToLogin.password === req.body.password){
+      res.send('you are logged in')
+    }else{
+      res.send("incorrect password")
+    }
+  }
+  
 })
 
 
-app.post("/signup", (req,res) =>{
+app.post("/signup",async (req,res) =>{
   console.log(req.body);
-  res.send(req.body)
+  if(req.body.username && req.body.password){
+  let newUser = await User.create(req.body)
+  res.send(newUser) 
+  }
+  
 })
 
 
