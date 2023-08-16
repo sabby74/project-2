9d2c2ebc18be7444fa4601341ba8ad30fa1d4a65
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2023-08-16 02:54:53 -0400

    wrkng on buttons appjs

--- public/app.js
+++ public/app.js
@@ -1,1 +1,21 @@
 console.log("hello there, welcome to Soumyas online store. ");
+
+const order= {
+    products:[]
+}
+const allButtons= document.querySelectorAll(".addToOrderBtn")
+
+
+allButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      const productId = e.srcElement.id
+      order.products.push(productId)
+    });
+  });
+
+
+
+
+const submit = document.querySelector("#submit")
+
+console.log(submit);
